170fb20b95f87a7b2d9ece36d682d03a1f867e39
Michael Hardcastle <maaicle@gmail.com>
2022-07-17 09:34:04 -0600

    Created PostService/Impl and PostController

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,40 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+    // If a class is a spring bean, and it only has one constructor, we can omit the @Autowired annotation.
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //Convert DTO into entity
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post newPost = postRepository.save(post);
+
+        //Convert entity to DTO
+        PostDto postResponse = new PostDto();
+        postResponse.setId(newPost.getId());
+        postResponse.setTitle(newPost.getTitle());
+        postResponse.setDescription(newPost.getDescription());
+        postResponse.setContent(newPost.getContent());
+
+
+
+        return postResponse;
+    }
+}
