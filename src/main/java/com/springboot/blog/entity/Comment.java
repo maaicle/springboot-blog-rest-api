5a7a02e16125176975654743402856c8b6dc277d
Michael Hardcastle <maaicle@gmail.com>
2022-09-06 15:56:38 -0600

    Created CommnetRepository and CommentDto

--- src/main/java/com/springboot/blog/entity/Comment.java
+++ src/main/java/com/springboot/blog/entity/Comment.java
@@ -19,7 +19,6 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String name;
     private String email;
     private String body;
