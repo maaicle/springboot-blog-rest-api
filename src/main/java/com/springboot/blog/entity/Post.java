0da3208c8e9668594adbd1c797f1284a3c22aebc
Michael Hardcastle <maaicle@gmail.com>
2022-09-06 15:29:12 -0600

    Added Comment Entity

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -28,5 +30,8 @@ public class Post {
 
     @Column(nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 
 }
